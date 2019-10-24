ec7ec9595318cbce70d0ec4a93cb5846409c6a48
Ilya Kosterin <ilya@kostech.ru>
2019-10-24 13:27:09 +0300

    Домашнее задание, поравил задачу 1.

--- lesson1/js/l1t1.js
+++ lesson1/js/l1t1.js
@@ -1,4 +1,7 @@
-'use strict'
-let Tc = prompt ();
-let Tf = (9 / 5) * Tc + 32;
-alert(Tf);
+'use strict';
+window.onload = ()  =>{
+    let Tc = prompt ();
+    let Tf = (9 / 5) * Tc + 32;
+    alert(Tf);
+};
+
